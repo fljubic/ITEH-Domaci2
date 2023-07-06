861fb8a8a0ccefb19122e41299901cc7f3489007
Filip Ljubic <fljubic00@gmail.com>
2023-07-06 02:10:54 +0200

    Kreiranje Modela i Migracija

---
+++ drugi-domaci/database/migrations/2023_07_06_000247_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('to_do_list_id')->constrained('to_do_lists')->onDelete('cascade');
+            $table->string('description');
+            $table->date('due_date')->nullable();
+            $table->string('done');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
